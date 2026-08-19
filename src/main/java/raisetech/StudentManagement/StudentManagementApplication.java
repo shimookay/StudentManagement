0f0acc541be80af97728c4a8727c1fc689e21683
shimookay <l2620980953as@gmail.com>
2026-08-20 00:12:56 +0900

    講義18回（課題10回）作成

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -4,12 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -23,35 +18,14 @@ public class StudentManagementApplication {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/student")
-	public String getStudent(@RequestParam String name) {
-		Student student = repository.searchByName(name);
-
-		if (student == null) {
-			return "該当する学生はいません。";
-		}
-
-		return student.getName() + " " + student.getAge() + "歳";
+	@GetMapping("/studentList")
+	public List<Student> getStudentList() {
+		return repository.search();
 	}
 
-	// 学生一覧取得
-	@GetMapping("/students")
-	public List<Student> getStudents() {
-		return repository.searchAll();
+	@GetMapping("/studentCoursesList")
+	public List<StudentCourses> getStudentCoursesList() {
+		return repository.searchCourses();
 	}
 
-  @PostMapping("/student")
-  public void registerStudent(String name, int age) {
-    repository.registerStudent(name, age);
-  }
-
-	@PatchMapping("/student")
-	public void updateStudent(String name, int age) {
-		repository.updateStudent(name, age);
-	}
-
-	@DeleteMapping("/student")
-	public void deleteStudent(String name) {
-		repository.deleteStudent(name);
-	}
 }
