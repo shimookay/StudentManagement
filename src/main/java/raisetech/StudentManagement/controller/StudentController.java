96b311db2e13b2854f44dd15666fb03dd120919a
shimookay <l2620980953as@gmail.com>
2026-09-16 19:29:41 +0900

    スタンダード第25回：第15回演習課題 15_Thymeleafを使ったPOST処理

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -6,7 +6,10 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import raisetech.StudentManagement.controller.converter.StudentConverter;
 import raisetech.StudentManagement.data.Student;
@@ -42,5 +45,27 @@ public class StudentController {
   public List<StudentCourses> getStudentCoursesList() {
     // リクエストの加工処理、入力チェック等
     return service.searchStudentCoursesList();
+  }
+
+  @GetMapping("/newStudent")
+  public String newStudent(Model model) {
+    model.addAttribute("studentDetail", new StudentDetail());
+    return "registerStudent";
+  }
+
+  @PostMapping("/registerStudent")
+  public String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
+    if(result.hasErrors()) {
+      return "registerStudent";
+    }
+    // System.out.println(studentDetail.getStudent().getName() + "さんが新規受講生として登録されました。");
+
+    // ①新規受講生情報を登録する処理を実装する。
+    // 　→ 登録された受講生情報が、結果としてstudentListとして検索画面に出てくるように実装する。
+    service.registerStudent(studentDetail.getStudent());
+
+    // ②コース情報も一緒に登録できるように実装する。登録するコース情報は１つで良い。
+
+    return "redirect:/studentList";
   }
 }
